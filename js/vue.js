b7030040a33e1412570ee6b42227bf1bbee0dce2
Antonio-Italiano <antoitalia221@gmail.com>
2023-01-13 15:25:43 +0100

    Add milestone 2-3

--- js/vue.js
+++ js/vue.js
@@ -1,18 +1,68 @@
-console.log('Vue ok', Vue);
+// console.log('Vue ok', Vue);
+const dateTime = luxon.DateTime;
 
 const app = Vue.createApp ({
     data() {
         return {
-
+            newMess: {
+                date: '10/01/2020 15:30:55',
+                text: '',
+                status: 'sent'
+              },
+            currentIndex: 0,
+            data,                        
         }
     },
     computed: {
-
+        currentContact() {
+            return this.data.contacts[this.currentIndex];
+        },
+        currentChat() {
+            return this.currentContact.messages;
+        }
+        
     },
     methods: {
+
+        buildAvatarUrl(avatar){
+            return `img/avatar${avatar}.jpg`
+        },
+
+        setCurrentIndex(index){
+            this.currentIndex = index;
+        },
+
+        addMessages(){
+                const mess = {...this.newMess};
+                this.currentChat.push(mess);
+                
+                this.currentContact.visible = setTimeout(() => {  
+                    const mess = { 
+                    date: '10/01/2020 15:30:55',
+                    text: 'Si capo',
+                    status: 'received' };
+                    this.currentChat.push(mess);
+                }, 2000);
+            
+            this.clearInput();
+        },
+
+        addMessagesRecived(){
+            
+        },
+
+        clearInput(){
+            this.newMess.text = '';
+        },
+
+        getCurrentData(){
+            return dateTime.now().setLocale('it').toLocaleString(dateTime.DATETIME_SHORT);
+        }
+
 
     }
 
 })
 
 app.mount('#root');
+
